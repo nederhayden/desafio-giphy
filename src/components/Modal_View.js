50cac635d164d700dc56833807d8e99da68267c7
Neder <nedersimoes@gmail.com>
2021-03-31 12:03:58 -0400

    personalizando modal

--- src/components/Modal_View.js
+++ src/components/Modal_View.js
@@ -5,7 +5,7 @@ export default function ModalView({ open, gif }) {
   const [close, setClose] = useState(false);
 
   return (
-    <Modal show={open} onHide={() => setClose(true)}>
+    <Modal dialogClassName='modal' show={open} onHide={() => setClose(true)} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton>
         <Modal.Body>
           <img className="img-gifs" src={gif.images.original.url} alt="" />
