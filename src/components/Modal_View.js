a639a7a3edd4cd9947afeba3bf139b1d6bb67739
Neder <nedersimoes@gmail.com>
2021-05-11 15:36:57 -0400

    custom modal

--- src/components/Modal_View.js
+++ src/components/Modal_View.js
@@ -13,22 +13,14 @@ export default function ModalView({ gif }) {
       onHide={() => setShowModal(!showModal)}
       centered
     >
-      <Modal.Body className="modal-body">
-        <img className="modal-img" src={gif.images.original.url} alt="" />
+      <Modal.Header className="modal-header" closeButton>
+        <img src={gif.images.original.url} alt="" />
         <ul>
           <li>Nome: {gif.username}</li>
           <li>Altura: {gif.images.original.height}</li>
           <li>Largura: {gif.images.original.width}</li>
         </ul>
-      </Modal.Body>
-      <Modal.Footer className="modal-footer">
-        <button
-          className="modal-button"
-          onClick={() => setShowModal(!showModal)}
-        >
-          <p>Close</p>
-        </button>
-      </Modal.Footer>
+      </Modal.Header>
     </Modal>
   );
 }
